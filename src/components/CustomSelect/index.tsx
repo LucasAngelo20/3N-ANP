6f4ce5616b5fc5367ee410f64561a10a34203f8d
Lucas Angelo <lucas.angelo@cyrela.com.br>
2023-06-08 02:34:07 -0300

    Ajuste das outras telas para a nova logica de inpu

--- src/components/CustomSelect/index.tsx
+++ src/components/CustomSelect/index.tsx
@@ -2,16 +2,17 @@ import { cpfmask } from "@/src/mists/cpfmask";
 import { TextField, colors } from "@mui/material";
 import { type } from "os";
 type DataProps = {
-  name: string
-}
+  name: string;
+};
 
 type CustomSelectProps = {
   label: string;
   placeholder?: string;
   fullWidth?: boolean;
-  width?: string
-  data?: Array<DataProps>
-  value?: string
+  width?: string;
+  data?: Array<DataProps>;
+  value?: string;
+  name?: string;
   onChange?: (value: any) => void;
 };
 
@@ -22,30 +23,33 @@ const CustomSelect = ({
   width,
   onChange,
   data,
-  value
+  value,
+  name
 }: CustomSelectProps) => {
   return (
     <div style={{ display: "flex", flexDirection: "column", margin: 10 }}>
-      <span style={{ fontSize: 14, fontWeight: 300, marginBottom: 5 }}>{label}</span>
+      <span style={{ fontSize: 14, fontWeight: 300, marginBottom: 5 }}>
+        {label}
+      </span>
       <select
         style={{
-          width: fullWidth ? "100%" : width ? width : '20%',
+          width: fullWidth ? "100%" : width ? width : "20%",
           height: 40,
           borderRadius: 5,
           outline: "none",
           border: "1px solid rgba(000,000,000,0.2)",
           fontSize: 14,
-          fontWeight: 300, 
-          color: 'rgba(000,000,000,0.6)'
+          fontWeight: 300,
+          color: "rgba(000,000,000,0.6)",
         }}
         placeholder={placeholder}
-        name={label}
+        name={name}
         value={value}
         onChange={onChange}
       >
-        {data?.map(data => (
+        {data?.map((data) => (
           <>
-          <option value={data?.name}>{data?.name}</option>
+            <option value={data?.name}>{data?.name}</option>
           </>
         ))}
       </select>
--- src/screens/EmissaoR1Pax/index.tsx
+++ src/screens/EmissaoR1Pax/index.tsx
@@ -5,7 +5,7 @@ import useSnackbar from "@/src/hooks/useSnackbar";
 import { cpfmask } from "@/src/mists/cpfmask";
 import { Box, Grid, Snackbar } from "@mui/material";
 import moment from "moment";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const dataEmpresas = [
   { name: "cadastro1" },
@@ -17,262 +17,193 @@ type SessionProps = {
   empresa: string;
 };
 
+const data = {
+  empresa: dataEmpresas[0].name,
+  nome: "",
+  cpf: "",
+  numeroDoContrato: "",
+  funcao: "",
+  origem: "",
+  destino: "",
+  data: Date(),
+  centroDeCusto: "",
+};
+
+const valueList = [data];
+
 const EmissaoR1Pax = () => {
-  const [empresa, setEmpresa] = useState(dataEmpresas[0].name);
-  const [cpf, setCpf] = useState("");
-  const [nome, setNome] = useState("");
-  const [contrato, setContrato] = useState("");
-  const [funcao, setFuncao] = useState("");
-  const [data, setData] = useState();
-  const [destino, setDestino] = useState("");
-  const [centroDeCusto, setCentroDeCusto] = useState("");
+  const [values, setvalues] = useState<any>(valueList);
   const [open, setOpen] = useState(false);
-  const [contadorEmissao, setContadorEmissao] = useState<Array<SessionProps>>(
-    []
-  );
   const [Info, setInfo] = useState<Array<SessionProps>>([]);
 
-  const { showSnackbar } = useSnackbar();
+  const ref = useRef(null);
 
   const handleSaveInfos = () => {
     console.log("Informações salvas: ");
-    console.log(empresa);
-    console.log(nome);
-    console.log(funcao);
-    console.log(contrato);
-    console.log(cpf);
-    console.log(data);
-    setInfo([
-      ...contadorEmissao,
-      {
-        empresa: empresa,
-      },
-    ]);
+    const addState = [...values];
+    setInfo(addState);
     setOpen(true);
+    console.log(addState);
   };
 
   const handleAddEmissao = () => {
-    if (contadorEmissao.length < 0) {
-      setContadorEmissao([
-        {
-          empresa: empresa,
-        },
-      ]);
-    } else {
-      setContadorEmissao([
-        ...contadorEmissao,
-        {
-          empresa: empresa,
-        },
-      ]);
-    }
-    console.log("Adicionado", contadorEmissao);
+    const addState = [...values, data];
+    setvalues(addState);
   };
+
   const handleRemoveEmissao = () => {
-    if (contadorEmissao.length === 1) {
-      setContadorEmissao([]);
-    } else if (contadorEmissao.length > 1) {
-      setContadorEmissao(contadorEmissao.slice(0, -1));
-    }
-    console.log("Adicionado", contadorEmissao);
+    setvalues(values.slice(0, -1));
+
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const handleChange = (event: any, index: any) => {
+    const { value, name } = event.target;
+    const newState = [...values];
+
+    newState[index] = {
+      ...newState[index],
+      [name]: value,
+    };
+
+    setvalues(newState);
   };
   return (
     <>
-      <Box>
-        <Grid container>
-          <Grid xs={6}>
-            <CustomSelect
-              width="44%"
-              label="Empresa:"
-              data={dataEmpresas}
-              value={empresa}
-              onChange={(e) => setEmpresa(e.target.value)}
-            />
-          </Grid>
-        </Grid>
-        <Grid container>
-          <Grid xs={3}>
-            <CustomTextArea
-              width="90%"
-              label="Nome:"
-              placeholder="Nome completo"
-              value={nome}
-              onChange={(e) => setNome(e.target.value)}
-            />
-          </Grid>
-          <Grid xs={3}>
-            <CustomTextArea
-              width="90%"
-              label="CPF:"
-              placeholder="000.000.000-00"
-              value={cpfmask(cpf)}
-              type="CPF"
-              onChange={(e) => setCpf(e.target.value)}
-            />
-          </Grid>
-          <Grid xs={3}>
-            <CustomTextArea
-              width="90%"
-              label="Numero do contrato:"
-              placeholder="00000"
-              value={contrato}
-              onChange={(e) => setContrato(e.target.value)}
-            />
-          </Grid>
-          <Grid xs={3}>
-            <CustomTextArea
-              width="90%"
-              label="Função:"
-              placeholder="00000"
-              value={funcao}
-              onChange={(e) => setFuncao(e.target.value)}
-            />
-          </Grid>
-          <Grid xs={3}>
-            <CustomTextArea
-              width="90%"
-              label="Origem:"
-              placeholder="00000"
-              value={funcao}
-              onChange={(e) => setFuncao(e.target.value)}
-            />
-          </Grid>
-          <Grid xs={3}>
-            <CustomTextArea
-              width="90%"
-              label="Destino:"
-              placeholder="00000"
-              value={destino}
-              onChange={(e) => setDestino(e.target.value)}
-            />
-          </Grid>
-          <Grid xs={3}>
-            <CustomTextArea
-              width="90%"
-              label="Data:"
-              placeholder="00/00/0000"
-              value={data}
-              fieldType="date"
-              onChange={(e) => setData(e.target.value)}
-            />
-          </Grid>
-          <Grid xs={3}>
-            <CustomTextArea
-              width="90%"
-              label="Centro de Custo:"
-              placeholder=""
-              value={centroDeCusto}
-              onChange={(e) => setCentroDeCusto(e.target.value)}
-            />
-          </Grid>
-        </Grid>
-      </Box>
-      {contadorEmissao.map((item) => (
-        <>
-          <div
-            style={{
-              borderTop: "1px solid rgba(000,000,000,0.2)",
-              margin: "20px 0",
-            }}
-          />
-          <Box>
-            <Grid container>
-              <Grid xs={6}>
-                <CustomSelect
-                  width="44%"
-                  label="Empresa:"
-                  data={dataEmpresas}
-                  value={empresa}
-                  onChange={(e) => setEmpresa(e.target.value)}
-                />
+      {values.map(
+        (
+          {
+            empresa,
+            nome,
+            cpf,
+            numeroDoContrato,
+            funcao,
+            origem,
+            destino,
+            data,
+            centroDeCusto,
+          }: any,
+          index: any
+        ) => (
+          <>
+            {index != 0 && <div
+              style={{
+                borderTop: "1px solid rgba(000,000,000,0.2)",
+                margin: "20px 0",
+              }}
+            />}
+            <Box>
+              <Grid container>
+                <Grid xs={6}>
+                  <CustomSelect
+                    width="44%"
+                    label="Empresa:"
+                    data={dataEmpresas}
+                    value={empresa}
+                    name="empresa"
+                    onChange={(event) => handleChange(event, index)}
+                  />
+                </Grid>
               </Grid>
-            </Grid>
-            <Grid container>
-              <Grid xs={3}>
-                <CustomTextArea
-                  width="90%"
-                  label="Nome:"
-                  placeholder="Nome completo"
-                  value={nome}
-                  onChange={(e) => setNome(e.target.value)}
-                />
+              <Grid container>
+                <Grid xs={3}>
+                  <CustomTextArea
+                    width="90%"
+                    label="Nome:"
+                    placeholder="Nome completo"
+                    value={nome}
+                    name="nome"
+                    onChange={(event) => handleChange(event, index)}
+                  />
+                </Grid>
+                <Grid xs={3}>
+                  <CustomTextArea
+                    width="90%"
+                    label="CPF:"
+                    placeholder="000.000.000-00"
+                    value={cpfmask(cpf)}
+                    type="CPF"
+                    name="cpf"
+                    onChange={(event) => handleChange(event, index)}
+                  />
+                </Grid>
+                <Grid xs={3}>
+                  <CustomTextArea
+                    width="90%"
+                    label="Numero do contrato:"
+                    placeholder="00000"
+                    value={numeroDoContrato}
+                    name="numeroDoContrato"
+                    onChange={(event) => handleChange(event, index)}
+                  />
+                </Grid>
+                <Grid xs={3}>
+                  <CustomTextArea
+                    width="90%"
+                    label="Função:"
+                    placeholder="00000"
+                    value={funcao}
+                    name="funcao"
+                    onChange={(event) => handleChange(event, index)}
+                  />
+                </Grid>
+                <Grid xs={3}>
+                  <CustomTextArea
+                    width="90%"
+                    label="Origem:"
+                    placeholder="00000"
+                    value={origem}
+                    name="origem"
+                    onChange={(event) => handleChange(event, index)}
+                  />
+                </Grid>
+                <Grid xs={3}>
+                  <CustomTextArea
+                    width="90%"
+                    label="Destino:"
+                    placeholder="00000"
+                    value={destino}
+                    name="destino"
+                    onChange={(event) => handleChange(event, index)}
+                  />
+                </Grid>
+                <Grid xs={3}>
+                  <CustomTextArea
+                    width="90%"
+                    label="Data:"
+                    placeholder="00/00/0000"
+                    value={data}
+                    fieldType="date"
+                    name="data"
+                    onChange={(event) => handleChange(event, index)}
+                  />
+                </Grid>
+                <Grid xs={3}>
+                  <CustomTextArea
+                    width="90%"
+                    label="Centro de Custo:"
+                    placeholder=""
+                    value={centroDeCusto}
+                    name="centroDeCusto"
+                    onChange={(event) => handleChange(event, index)}
+                  />
+                </Grid>
               </Grid>
-              <Grid xs={3}>
-                <CustomTextArea
-                  width="90%"
-                  label="CPF:"
-                  placeholder="000.000.000-00"
-                  value={cpfmask(cpf)}
-                  type="CPF"
-                  onChange={(e) => setCpf(e.target.value)}
-                />
-              </Grid>
-              <Grid xs={3}>
-                <CustomTextArea
-                  width="90%"
-                  label="Numero do contrato:"
-                  placeholder="00000"
-                  value={contrato}
-                  onChange={(e) => setContrato(e.target.value)}
-                />
-              </Grid>
-              <Grid xs={3}>
-                <CustomTextArea
-                  width="90%"
-                  label="Função:"
-                  placeholder="00000"
-                  value={funcao}
-                  onChange={(e) => setFuncao(e.target.value)}
-                />
-              </Grid>
-              <Grid xs={3}>
-                <CustomTextArea
-                  width="90%"
-                  label="Origem:"
-                  placeholder="00000"
-                  value={funcao}
-                  onChange={(e) => setFuncao(e.target.value)}
-                />
-              </Grid>
-              <Grid xs={3}>
-                <CustomTextArea
-                  width="90%"
-                  label="Destino:"
-                  placeholder="00000"
-                  value={destino}
-                  onChange={(e) => setDestino(e.target.value)}
-                />
-              </Grid>
-              <Grid xs={3}>
-                <CustomTextArea
-                  width="90%"
-                  label="Data:"
-                  placeholder="00/00/0000"
-                  value={data}
-                  fieldType="date"
-                  onChange={(e) => setData(e.target.value)}
-                />
-              </Grid>
-              <Grid xs={3}>
-                <CustomTextArea
-                  width="90%"
-                  label="Centro de Custo:"
-                  placeholder=""
-                  value={centroDeCusto}
-                  onChange={(e) => setCentroDeCusto(e.target.value)}
-                />
-              </Grid>
-            </Grid>
-          </Box>
-        </>
-      ))}
+            </Box>
+          </>
+        )
+      )}
 
       <Grid container>
         <Grid xs={2}>
           <CustomButton text="Adicionar" onClick={() => handleAddEmissao()} />
         </Grid>
         <Grid xs={2}>
-          <CustomButton display={contadorEmissao.length == 0} text="Remover" onClick={() => handleRemoveEmissao()} />
+          <CustomButton
+            display={values.length == 1}
+            text="Remover"
+            onClick={() => handleRemoveEmissao()}
+          />
         </Grid>
       </Grid>
       <Grid xs={6}>
