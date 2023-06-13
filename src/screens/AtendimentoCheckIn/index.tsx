b26fed00d53bc7e481df4294b8cec1a7ccb3ae75
Lucas Angelo <lucas.angelo@cyrela.com.br>
2023-06-13 02:14:33 -0300

    Ajustes atendimento checkin

--- src/screens/AtendimentoCheckIn/index.tsx
+++ src/screens/AtendimentoCheckIn/index.tsx
@@ -14,22 +14,33 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import moment from "moment";
 
-const dataEmpresas = [
-  { name: "cadastro1" },
-  { name: "cadastro2" },
-  { name: "cadastro3" },
+const dataTipo = [
+  { name: "pax1" },
+  { name: "pax2" },
+  { name: "pax3" },
 ];
 
 const AtendimentoCheckIn = () => {
-  const [empresa, setEmpresa] = useState(dataEmpresas[0].name);
+  const [empresa, setEmpresa] = useState(dataTipo[0].name);
   const [origem, setOrigem] = useState("");
   const [data, setData] = useState("");
-  const [filteredRows, setFilteredRows] = useState<any>([]);
+  const [eam, setEam] = useState<any>([]);
 
   const handleSaveInfos = () => {};
 
   return (
     <Box>
+      <Grid container>
+        <Grid xs={6}>
+          <CustomSelect
+            width="59.2%"
+            label="Tipo:"
+            data={dataTipo}
+            value={empresa}
+            onChange={(e) => setEmpresa(e.target.value)}
+          />
+        </Grid>
+      </Grid>
       <Grid container>
         <Grid xs={4}>
           <CustomTextArea
@@ -50,10 +61,21 @@ const AtendimentoCheckIn = () => {
             onChange={(e) => setOrigem(e.target.value)}
           />
         </Grid>
+        <Grid xs={4}>
+          <CustomTextArea
+            width="90%"
+            label="EAM:"
+            placeholder=""
+            value={eam}
+            onChange={(e) => setEam(e.target.value)}
+          />
+        </Grid>
       </Grid>
       <Grid container>
         <Grid xs={6} minWidth="100%" display="flex">
-          <CustomButton text="Pesquidar" onClick={() => handleSaveInfos()} />
+          <CustomButton text="Relatorio" onClick={() => handleSaveInfos()} />
+          <CustomButton text="Conferir" onClick={() => handleSaveInfos()} />
+          <CustomButton text="Exportar" onClick={() => handleSaveInfos()} />
         </Grid>
       </Grid>
     </Box>
